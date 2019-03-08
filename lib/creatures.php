c50a263d4632c8884f48621f69f2551b28da112b
Ondrej Zara <ondrej.zara@gmail.com>
2019-03-08 12:54:10 +0100

    creatures

--- lib/creatures.php
+++ lib/creatures.php
@@ -7,6 +7,24 @@ $creature_templates = array(
 		"color" => array("h"=> 30, "s"=>array(80, 100), "v"=>array(50, 90))
 	),
 
+	"centipede" => array(
+		"letter" => "c",
+		"name" => "Centipede",
+		"color" => array("h"=> array(30, 50), "s"=>array(50, 90), "v"=>array(60, 90))
+	),
+
+	"demon" => array(
+		"letter" => "d",
+		"name" => "demon",
+		"color" => array("h"=> 0, "s"=>array(80, 100), "v"=>array(70, 100))
+	),
+
+	"eye" => array(
+		"letter" => "e",
+		"name" => "Evil Eye",
+		"color" => array("h"=> array(170, 270), "s"=>array(90, 100), "v"=>array(80, 100))
+	),
+
 	"goblin" => array(
 		"letter" => "g",
 		"name" => "Goblin",
@@ -19,16 +37,40 @@ $creature_templates = array(
 		"color" => array("h"=> array(160, 180), "s"=>array(80, 100), "v"=>array(80, 100))
 	),
 
+	"imp" => array(
+		"letter" => "i",
+		"name" => "Imp",
+		"color" => array("h"=> array(300, 360), "s"=>array(90, 100), "v"=>array(90, 100))
+	),
+
 	"kobold" => array(
 		"letter" => "k",
 		"name" => "Kobold",
 		"color" => array("h"=>0, "s"=>0, "v"=>array(30, 70))
 	),
 
+	"lion" => array(
+		"letter" => "l",
+		"name" => "Lion",
+		"color" => array("h"=>array(30, 50), "s"=>array(90, 100), "v"=>array(80, 90))
+	),
+
+	"mummy" => array(
+		"letter" => "m",
+		"name" => "Mummy",
+		"color" => array("h"=>0, "s"=>0, "v"=>array(80, 100))
+	),
+
 	"orc" => array(
 		"letter" => "o",
 		"name" => "Orc",
 		"color" => array("h"=> array(180, 250), "s"=>array(80, 100), "v"=>90)
+	),
+
+	"phantom" => array(
+		"letter" => "p",
+		"name" => "Phantom",
+		"color" => array("h"=> array(300, 330), "s"=>array(30, 40), "v"=>array(90, 100))
 	),
 
 	"rat" => array(
@@ -41,6 +83,18 @@ $creature_templates = array(
 		"letter" => "s",
 		"name" => "Snake",
 		"color" => array("h"=> array(70, 160), "s"=>array(80, 100), "v"=>70)
+	),
+
+	"troll" => array(
+		"letter" => "t",
+		"name" => "Troll",
+		"color" => array("h"=> array(30, 50), "s"=>array(40, 90), "v"=>70)
+	),
+
+	"vampire" => array(
+		"letter" => "v",
+		"name" => "Vampire",
+		"color" => array("h"=> 300, "s"=>array(70, 100), "v"=>array(80, 100))
 	),
 
 	"wizard" => array(
